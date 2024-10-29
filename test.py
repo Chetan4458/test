cf365a985129772e63e03804650be13d53269b52
Chetan4458 <111554115+Chetan4458@users.noreply.github.com>
2024-10-29 15:16:11 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,16 +3,16 @@ def example_function():
     print("This is a sample function")
 
 def another_function():
-    for i in range(10)  # Missing colon
+    for i in range(10) : # Missing colon
         print(i)
 
     if i > 5:  # Unused variable 'i'
-        print("i is greater than 5"  # Missing closing parenthesis
-    else
+        print("i is greater than 5" ) # Missing closing parenthesis
+    else:
         print("i is less than or equal to 5")  # Missing colon
 
 example_function()
-another_function(10)  # Incorrect number of arguments
+#another_function(10)  # Incorrect number of arguments
 
 # Logical error example
-result = "5" + 5  # This will raise a TypeError
+result = 5  # This will raise a TypeError
