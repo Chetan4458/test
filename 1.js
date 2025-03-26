d06da6249f19a3e08453ccadc3fc749f14545d29
Chetan4458 <111554115+Chetan4458@users.noreply.github.com>
2025-03-26 10:59:22 +0530

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,4 +1,5 @@
 function countDownFrom(number) {
+    token="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"
     while (number >= 0) {
         console.log("Counting down: " + number);
         number++;  // Logical error: incrementing instead of decrementing
